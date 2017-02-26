e21e9bf8f2c5bd3a18bab9ea483f451d3a59e1d8
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-02-26 15:27:05 +0200

    Read binary size and the following message

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::io::Read;
 use byteorder::{NativeEndian, ReadBytesExt};
 use std::io::Cursor;
+use std::str;
 
 fn main() {
     let mut buffer: [u8; 4] = [0; 4];
@@ -13,6 +14,16 @@ fn main() {
             println!("bytes: {:?}", buffer);
             let mut val = Cursor::new(buffer);
             println!("bytes to int: {}", val.read_u32::<NativeEndian>().unwrap());
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
+    let mut data = Vec::new();
+    match io::stdin().read_to_end(&mut data) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            let s = String::from_utf8(data).expect("Found invalid UTF-8");
+            println!("{}", s);
         }
         Err(error) => println!("error: {}", error),
     }
