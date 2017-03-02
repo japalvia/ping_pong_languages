b54a430555b3125716b52fa8296a5ac1dc94865e
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-03-02 19:46:31 +0200

    Debug() log function to stderr.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate byteorder;
+extern crate serde_json;
 
 use std::io;
 use std::io::{Read, Write};
 use byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
 use std::io::Cursor;
-use std::str;
 
 fn send_message() {
    // let mut c = Cursor::new(bytes);
@@ -18,24 +18,28 @@ fn send_message() {
    io::stdout().flush().unwrap();
 }
 
+fn debug(s: String) {
+    writeln!(&mut std::io::stderr(), "{}", s).unwrap();
+}
+
 fn main() {
     let mut bytes: [u8; 4] = [0; 4];
     io::stdin().read(&mut bytes).unwrap();
     let mut c = Cursor::new(bytes);
     let len = c.read_u32::<NativeEndian>().unwrap();
-    // println!("len: {}", len);
+    debug(format!("len: {}", len));
 
     // Note: read() requires pre-sized vector
     let mut buf = vec![0; len as usize];
     let mut handle = io::stdin().take(len as u64);
-    let n = handle.read(&mut buf).unwrap();
-    // println!("n: {}", n);
+    handle.read(&mut buf).unwrap();
 
     let msg = String::from_utf8(buf).unwrap();
-    // println!("message: {}", msg);
-
+    debug(format!("message: {}", msg));
+    /*
     if msg == "ping" {
         // println!("we got ping");
         send_message();
     }
+*/
 }
