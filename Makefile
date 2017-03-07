f0418e5f38f4cf49bbf960915870bb7179f99de9
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-03-07 19:45:43 +0200

    Decode json request.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gcc -Wall -std=c99 -pedantic -g main.c -o main
+	gcc -Wall -std=c99 -pedantic -g -ljansson main.c -o main
 
 clean:
 	rm -rf main
--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <string.h>
+#include <jansson.h>
 
 void debug(const char *fmt, ...)
 {
@@ -31,7 +33,7 @@ void debug(const char *fmt, ...)
 	}
 	va_end(ap);
 
-	fprintf(stderr, "%s\n", p);
+	fprintf(stderr, "%s", p);
 	free(p);
 }
 
@@ -44,24 +46,50 @@ int bin2int(unsigned char *bytes) {
 	return val;
 }
 
+const char *receive_message() {
+
+}
+
 int main(int argc, char **argv) {
 	unsigned char buf[4] = {0};
 
 	fread(buf, sizeof(buf[0]), sizeof(buf), stdin);
 	unsigned int len = bin2int(buf);
-	debug("%d", len);
+	debug("%d\n", len);
 
-	char *data = calloc(len + 1, sizeof(char));
-	if (!data) {
-		debug("calloc() failed");
+	char *text = calloc(len + 1, sizeof(char));
+	if (!text) {
+		debug("calloc() failed\n");
 		exit(1);
 	}
 
-    fread(data, sizeof(data[0]), len, stdin);
-    debug("data: %s\n", data);
+	fread(text, sizeof(text[0]), len, stdin);
+	debug("text: %s\n", text);
 
+	json_t *root;
+	json_error_t error;
 
-    free(data);
+	root = json_loads(text, 0, &error);
+	free(text);
+
+    if(!root) {
+		debug("error: on line %d: %s\n", error.line, error.text);
+		return 1;
+	}
+
+    if(!json_is_array(root)) {
+		debug("error: root is not an array\n");
+		json_decref(root);
+		return 1;
+	}
+
+    json_t *data = json_array_get(root, 0);
+    if(json_is_string(data) &&
+            strcmp(json_string_value(data), "ping") == 0) {
+        debug("we got string\n");
+    }
+
+	json_decref(root);
 
 	return 0;
 }
