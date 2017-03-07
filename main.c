eae253c1d79fdf2d7c9df58a227775ba371e505d
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-03-07 21:18:34 +0200

    Working receive message and sending response.

--- main.c
+++ main.c
@@ -46,11 +46,19 @@ int bin2int(unsigned char *bytes) {
 	return val;
 }
 
-const char *receive_message() {
+unsigned char *int2bin(unsigned int n) {
+	unsigned char *bytes;
+	bytes = calloc(4, sizeof(*bytes));
 
+	bytes[0] = n & 0xFF;
+	bytes[1] = (n >> 8) & 0xFF;
+	bytes[2] = (n >> 16) & 0xFF;
+	bytes[3] = (n >> 24) & 0xFF;
+
+	return bytes;
 }
 
-int main(int argc, char **argv) {
+char *receive_message() {
 	unsigned char buf[4] = {0};
 
 	fread(buf, sizeof(buf[0]), sizeof(buf), stdin);
@@ -64,6 +72,30 @@ int main(int argc, char **argv) {
 	}
 
 	fread(text, sizeof(text[0]), len, stdin);
+	return text;
+}
+
+void send_message() {
+	debug("send_message\n");
+	json_t *array = json_array();
+	json_array_append_new(array, json_string("pong"));
+
+	char *data = json_dumps(array, 0);
+	json_decref(array);
+
+	size_t len = strlen(data);
+	debug("str: %s, len: %d\n", data, len);
+	unsigned char *bytes = int2bin(len);
+	fwrite(bytes, sizeof(*bytes), 4, stdout);
+	free(bytes);
+
+	fwrite(data, sizeof(*data), len, stdout);
+	free(data);
+}
+
+int main(int argc, char **argv) {
+
+	char *text = receive_message();
 	debug("text: %s\n", text);
 
 	json_t *root;
@@ -72,25 +104,19 @@ int main(int argc, char **argv) {
 	root = json_loads(text, 0, &error);
 	free(text);
 
-    if(!root) {
-		debug("error: on line %d: %s\n", error.line, error.text);
-		return 1;
-	}
-
-    if(!json_is_array(root)) {
-		debug("error: root is not an array\n");
+	// Look for 'ping', ignore unexpected json
+	// but ensure still crash safety.
+	if(root) {
+		if(json_is_array(root)) {
+			json_t *data = json_array_get(root, 0);
+			if(json_is_string(data)) {
+				if(strcmp(json_string_value(data), "ping") == 0) {
+					send_message();
+				}
+			}
+		}
 		json_decref(root);
-		return 1;
 	}
-
-    json_t *data = json_array_get(root, 0);
-    if(json_is_string(data) &&
-            strcmp(json_string_value(data), "ping") == 0) {
-        debug("we got string\n");
-    }
-
-	json_decref(root);
-
 	return 0;
 }
 
