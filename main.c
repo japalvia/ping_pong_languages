7440d325a62cedb8b8f394367b47305eb00376c3
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-03-07 22:06:13 +0200

    Cleanup weird indentation.

--- main.c
+++ main.c
@@ -7,90 +7,90 @@
 
 void debug(const char *fmt, ...)
 {
-	int size = 0;
-	char *p = NULL;
-	va_list ap;
+    int size = 0;
+    char *p = NULL;
+    va_list ap;
 
-	/* Determine required size */
+    /* Determine required size */
 
-	va_start(ap, fmt);
-	size = vsnprintf(p, size, fmt, ap);
-	va_end(ap);
+    va_start(ap, fmt);
+    size = vsnprintf(p, size, fmt, ap);
+    va_end(ap);
 
-	if (size < 0)
-		return;
+    if (size < 0)
+        return;
 
-	size++;             /* For '\0' */
-	p = malloc(size);
-	if (p == NULL)
-		return;
+    size++;             /* For '\0' */
+    p = malloc(size);
+    if (p == NULL)
+        return;
 
-	va_start(ap, fmt);
-	size = vsnprintf(p, size, fmt, ap);
-	if (size < 0) {
-		free(p);
-		return;
-	}
-	va_end(ap);
+    va_start(ap, fmt);
+    size = vsnprintf(p, size, fmt, ap);
+    if (size < 0) {
+        free(p);
+        return;
+    }
+    va_end(ap);
 
-	fprintf(stderr, "%s", p);
-	free(p);
+    fprintf(stderr, "%s", p);
+    free(p);
 }
 
 int bin2int(unsigned char *bytes) {
-	int val = bytes[0] \
-			  | ( (int)bytes[1] << 8 ) \
-			  | ( (int)bytes[2] << 16 ) \
-			  | ( (int)bytes[3] << 24 );
+    int val = bytes[0] \
+              | ( (int)bytes[1] << 8 ) \
+              | ( (int)bytes[2] << 16 ) \
+              | ( (int)bytes[3] << 24 );
 
-	return val;
+    return val;
 }
 
 unsigned char *int2bin(unsigned int n) {
-	unsigned char *bytes;
-	bytes = calloc(4, sizeof(*bytes));
+    unsigned char *bytes;
+    bytes = calloc(4, sizeof(*bytes));
 
-	bytes[0] = n & 0xFF;
-	bytes[1] = (n >> 8) & 0xFF;
-	bytes[2] = (n >> 16) & 0xFF;
-	bytes[3] = (n >> 24) & 0xFF;
+    bytes[0] = n & 0xFF;
+    bytes[1] = (n >> 8) & 0xFF;
+    bytes[2] = (n >> 16) & 0xFF;
+    bytes[3] = (n >> 24) & 0xFF;
 
-	return bytes;
+    return bytes;
 }
 
 char *receive_message() {
-	unsigned char buf[4] = {0};
+    unsigned char buf[4] = {0};
 
-	fread(buf, sizeof(buf[0]), sizeof(buf), stdin);
-	unsigned int len = bin2int(buf);
-	debug("%d\n", len);
+    fread(buf, sizeof(buf[0]), sizeof(buf), stdin);
+    unsigned int len = bin2int(buf);
+    debug("%d\n", len);
 
-	char *text = calloc(len + 1, sizeof(char));
-	if (!text) {
-		debug("calloc() failed\n");
-		exit(1);
-	}
+    char *text = calloc(len + 1, sizeof(char));
+    if (!text) {
+        debug("calloc() failed\n");
+        exit(1);
+    }
 
-	fread(text, sizeof(text[0]), len, stdin);
-	return text;
+    fread(text, sizeof(text[0]), len, stdin);
+    return text;
 }
 
 void send_message() {
-	debug("send_message\n");
-	json_t *array = json_array();
-	json_array_append_new(array, json_string("pong"));
+    debug("send_message\n");
+    json_t *array = json_array();
+    json_array_append_new(array, json_string("pong"));
 
-	char *data = json_dumps(array, 0);
-	json_decref(array);
+    char *data = json_dumps(array, 0);
+    json_decref(array);
 
-	size_t len = strlen(data);
-	debug("str: %s, len: %d\n", data, len);
-	unsigned char *bytes = int2bin(len);
-	fwrite(bytes, sizeof(*bytes), 4, stdout);
-	free(bytes);
+    size_t len = strlen(data);
+    debug("str: %s, len: %d\n", data, len);
+    unsigned char *bytes = int2bin(len);
+    fwrite(bytes, sizeof(*bytes), 4, stdout);
+    free(bytes);
 
-	fwrite(data, sizeof(*data), len, stdout);
-	free(data);
+    fwrite(data, sizeof(*data), len, stdout);
+    free(data);
 }
 
 int main(int argc, char **argv) {
